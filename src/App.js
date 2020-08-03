42890f8aa20b8ff36a9ff104f52a61aa059cc3f7
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-08-02 23:45:56 -0400

    add firebase with no default project, add redux store file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { ThemeProvider, Button } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core';
 import theme from './theme';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/layouts/Login';
+import Dashboard from './components/layouts/Dashboard';
+import Register from './components/layouts/Register';
+import AppNavbar from './components/layouts/AppNavbar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
